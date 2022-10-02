cbf55fb29ccce13aa2c7f25a6c714fd90b876648
Gabriel Tavares <gtavares.linux@gmail.com>
2022-10-02 19:51:52 +0000

    adding greetings

--- aaliyah.py
+++ aaliyah.py
@@ -6,6 +6,8 @@
 
 import speech_recognition as sr
 import pyttsx3
+import datetime
+import time
 
 # speech-to-text using Microsoft speech (sapi5)
 engine = pyttsx3.init('sapi5')
@@ -15,3 +17,27 @@ engine.setProperty('voice',voices[1].id)
 def speak(audio):
     engine.say(audio)
     engine.runAndWait()
+
+# greeting function
+def wishMe():
+    hour = int(datetime.datetime.now().hour)
+    if hour >= 0 and hour < 12:
+        speak('Good Morning!')
+    
+    elif hour >= 12 and hour < 18:
+        speak('Good Afternoon!')
+
+    else:
+        speak('Good Evening')
+    
+    assname = ('Aaliyah 1 point 0')
+    speak('I am your AI')
+    speak(assname)
+
+# remember name
+def username():
+    speak('What shall I call you?')
+    uname = takeCommand()
+    speak('welcome')
+    speak(uname)
+    speak('How can I help you?')
